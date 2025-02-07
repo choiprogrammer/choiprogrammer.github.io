4f8e4d0238ea893259bf5f586eaa3c3eb1cf9c21
choiprogrammer <raphaeldkm@gmail.com>
2025-02-08 07:59:54 +0800

    Previous Commit didn't include the changes for index.html, styles.css and script.js. Woops. Here it is.

--- TS1/js/script.js
+++ TS1/js/script.js
@@ -1,5 +1,10 @@
-function changeColor() {
-    const colors = ['#f0f8ff', '#faebd7', '#7fffd4', '#ffe4c4', '#ffebcd'];
-    const randomColor = colors[Math.floor(Math.random() * colors.length)];
-    document.body.style.backgroundColor = randomColor;
+function toggleDarkMode() {
+    const body = document.body;
+    const button = document.querySelector('button');
+    body.classList.toggle('dark-mode');
+    if (body.classList.contains('dark-mode')) {
+        button.textContent = 'Bright Mode';
+    } else {
+        button.textContent = 'Dark Mode';
+    }
 }
